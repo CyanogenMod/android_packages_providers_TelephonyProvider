c51b725f3d3b598857aab6204f97a2f049c7017a
Steve Kondik <shade@chemlab.org>
2012-05-21 23:24:06 -0700

    telephony: Add support for choosing a default APN
    * Sometimes, the standard method of choosing the "first" available APN
       is not what we want. Consider the case where a certain provider has
       both a "normal" APN and an LTE APN. The LTE APN is what we want, but
       it's not chosen.
    * Add a new overlayable value "config_preferred_apn" to handle this. A
       device can override this. The format is "apn,mcc,mnc".
    
    Conflicts:
    	res/values/config.xml
    	src/com/android/providers/telephony/TelephonyProvider.java
    
    Change-Id: Ia6ca4159491bee15b3f18ad7ad524b8b0ffce2f2

--- src/com/android/providers/telephony/TelephonyProvider.java
+++ src/com/android/providers/telephony/TelephonyProvider.java
@@ -279,6 +279,24 @@ public class TelephonyProvider extends ContentProvider
             if (DBG) log("dbh.createCarriersTable:-");
         }
 
+        private int getDefaultPreferredApnId(SQLiteDatabase db) {
+            int id = -1;
+            String configPref = mContext.getResources().getString(R.string.config_preferred_apn, "");
+            if (!TextUtils.isEmpty(configPref)) {
+                String[] s = configPref.split(",");
+                if (s.length == 3) {
+                    Cursor c = db.query("carriers", new String[] { "_id" },
+                            "apn='" + s[0] + "' AND mcc='" + s[1] + "' AND mnc='" + s[2] + "'",
+                            null, null, null, null);
+                    if (c.moveToFirst()) {
+                        id = c.getInt(0);
+                    }
+                    c.close();
+                }
+            }
+            return id;
+        }
+
         /**
          *  This function adds APNs from xml file(s) to db. The db may or may not be empty to begin
          *  with.
@@ -1383,7 +1401,33 @@ public class TelephonyProvider extends ContentProvider
     private long getPreferredApnId(int subId) {
         SharedPreferences sp = getContext().getSharedPreferences(
                 PREF_FILE, Context.MODE_PRIVATE);
-        return sp.getLong(COLUMN_APN_ID + subId, -1);
+        long id = sp.getLong(COLUMN_APN_ID + subId, -1);
+        if (id == -1) {
+            id = getDefaultPreferredApnId();
+            if (id > -1) {
+                setPreferredApnId(id, subId);
+            }
+        }
+        return id;
+    }
+
+    private long getDefaultPreferredApnId() {
+        long id = -1;
+        String configPref = getContext().getResources().getString(R.string.config_preferred_apn, "");
+        if (!TextUtils.isEmpty(configPref)) {
+            String[] s = configPref.split(",");
+            if (s.length == 3) {
+                Cursor c = mOpenHelper.getReadableDatabase().query("carriers", new String[] { "_id" },
+                        "apn='" + s[0] + "' AND mcc='" + s[1] + "' AND mnc='" + s[2] + "'",
+                        null, null, null, null);
+                if (c.moveToFirst()) {
+                    id = c.getLong(0);
+                }
+                c.close();
+            }
+        }
+        Log.d(TAG, "Preferred APN: " + id);
+        return id;
     }
 
     private void deletePreferredApnId() {
@@ -2005,6 +2049,7 @@ public class TelephonyProvider extends ContentProvider
         }
         setPreferredApnId((long)-1, subId);
         mOpenHelper.initDatabase(db);
+        setPreferredApnId(getDefaultPreferredApnId(), subId);
     }
 
     private synchronized void updateApnDb() {
