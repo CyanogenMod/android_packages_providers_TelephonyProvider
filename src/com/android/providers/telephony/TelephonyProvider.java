07f61172905eb6be116b3f3126426f60cedd00ea
lrichard <richardl@codeaurora.org>
2013-12-16 14:52:59 +0800

    TelephonyProvider: Display APN PPP number
    Add a new column "ppp_number" to the carriers database for
    displaying the APN PPP number.
    
    Change-Id: Ib1b742257aa1ddfba12f0bfc42e476fca1dcd8c6

--- src/com/android/providers/telephony/TelephonyProvider.java
+++ src/com/android/providers/telephony/TelephonyProvider.java
@@ -154,7 +154,8 @@ public class TelephonyProvider extends ContentProvider
                     "bearer INTEGER," +
                     "mvno_type TEXT," +
                     "mvno_match_data TEXT," +
-                    "preferred BOOLEAN DEFAULT 0);");
+                    "preferred BOOLEAN DEFAULT 0," +
+                    "ppp_number TEXT);");
 
             initDatabase(db);
         }
@@ -315,6 +316,8 @@ public class TelephonyProvider extends ContentProvider
             map.put(Telephony.Carriers.USER, parser.getAttributeValue(null, "user"));
             map.put(Telephony.Carriers.SERVER, parser.getAttributeValue(null, "server"));
             map.put(Telephony.Carriers.PASSWORD, parser.getAttributeValue(null, "password"));
+            map.put(mContext.getString(R.string.ppp_number),
+                             parser.getAttributeValue(null, "ppp_number"));
 
             // do not add NULL to the map so that insert() will set the default value
             String proxy = parser.getAttributeValue(null, "proxy");
