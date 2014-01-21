78859025b1909a9cf93f97433ae471effd439986
Danny Baumann <dannybaumann@web.de>
2014-01-21 10:28:25 +0100

    Properly handle alphanumeric 'numbers'.
    Change-Id: I326198c19bb567846d09f8bb8c293a024c830296
    JIRA:CYAN-3234

--- src/com/android/providers/telephony/BlacklistProvider.java
+++ src/com/android/providers/telephony/BlacklistProvider.java
@@ -29,6 +29,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
 import android.provider.Telephony.Blacklist;
+import android.telephony.PhoneNumberUtils;
 import android.text.TextUtils;
 import android.util.Log;
 
@@ -37,7 +38,7 @@ public class BlacklistProvider extends ContentProvider {
     private static final boolean DEBUG = true;
 
     private static final String DATABASE_NAME = "blacklist.db";
-    private static final int DATABASE_VERSION = 2;
+    private static final int DATABASE_VERSION = 3;
 
     private static final String BLACKLIST_TABLE = "blacklist";
     private static final String COLUMN_NORMALIZED = "normalized_number";
@@ -89,6 +90,36 @@ public class BlacklistProvider extends ContentProvider {
                 onCreate(db);
                 db.execSQL("INSERT INTO " + BLACKLIST_TABLE +
                         " SELECT * FROM " + BLACKLIST_TABLE + "_old;");
+            }
+
+            if (oldVersion < 3) {
+                // update the normalized number column, v1 and v2 didn't handle
+                // alphanumeric 'numbers' correctly
+
+                Cursor rows = db.query(BLACKLIST_TABLE,
+                        new String[] { Blacklist._ID, Blacklist.NUMBER },
+                        null, null, null, null, null);
+
+                try {
+                    db.beginTransaction();
+                    if (rows != null) {
+                        ContentValues cv = new ContentValues();
+                        String[] rowId = new String[1];
+
+                        while (rows.moveToNext()) {
+                            rowId[0] = rows.getString(0);
+                            cv.clear();
+                            cv.put(COLUMN_NORMALIZED, normalizeNumber(rows.getString(1)));
+                            db.update(BLACKLIST_TABLE, cv, Blacklist._ID + "= ?", rowId);
+                        }
+                    }
+                    db.setTransactionSuccessful();
+                } finally {
+                    db.endTransaction();
+                    if (rows != null) {
+                        rows.close();
+                    }
+                }
             }
         }
     }
@@ -340,9 +371,9 @@ public class BlacklistProvider extends ContentProvider {
         mBackupManager.dataChanged();
     }
 
-    // mostly a copy of PhoneNumberUtils.normalizeNumber, with the exception of
-    // not converting characters and support for regex characters
-    private String normalizeNumber(String number) {
+    // mostly a copy of PhoneNumberUtils.normalizeNumber,
+    // with the exception of support for regex characters
+    private static String normalizeNumber(String number) {
         int len = number.length();
         StringBuilder ret = new StringBuilder(len);
 
@@ -352,6 +383,8 @@ public class BlacklistProvider extends ContentProvider {
             int digit = Character.digit(c, 10);
             if (digit != -1) {
                 ret.append(digit);
+            } else if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
+                return normalizeNumber(PhoneNumberUtils.convertKeypadLettersToDigits(number));
             } else if (i == 0 && c == '+') {
                 ret.append(c);
             } else if (c == '*') {
