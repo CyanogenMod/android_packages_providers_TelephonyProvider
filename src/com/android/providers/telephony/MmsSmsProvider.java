9fe49ac343250b69c4d9c864802ebfb4cad4e111
kaiyiz <kaiyiz@codeaurora.org>
2014-03-12 10:39:19 +0800

    Mms: Only match the first character when searching for message
    The original SQL statement only match the first character of
    message content.
    
    Modify the SQL statement to match the entire message content.
    
    CRs-Fixed: 629502
    
    Change-Id: I57500c99adf8cfb48ad941c4f490cb4ea636b2b3

--- src/com/android/providers/telephony/MmsSmsProvider.java
+++ src/com/android/providers/telephony/MmsSmsProvider.java
@@ -176,8 +176,8 @@ public class MmsSmsProvider extends ContentProvider {
     private static final String[] EMPTY_STRING_ARRAY = new String[0];
 
     private static final String[] SEARCH_STRING = new String[1];
-    private static final String SEARCH_QUERY = "SELECT snippet(words, '', ' ', '', 1, 1) as " +
-            "snippet FROM words WHERE index_text MATCH ? ORDER BY snippet LIMIT 50;";
+    private static final String SEARCH_QUERY = "SELECT index_text as snippet FROM " +
+            "words WHERE index_text like ? ORDER BY snippet LIMIT 50;";
 
     private static final String SMS_CONVERSATION_CONSTRAINT = "(" +
             Sms.TYPE + " != " + Sms.MESSAGE_TYPE_DRAFT + ")";
@@ -191,7 +191,7 @@ public class MmsSmsProvider extends ContentProvider {
     // Search on the words table but return the rows from the corresponding sms table
     private static final String SMS_QUERY =
             "SELECT sms._id AS _id,thread_id,address,body,date,date_sent,index_text,words._id " +
-            "FROM sms,words WHERE (index_text MATCH ? " +
+            "FROM sms,words WHERE (index_text LIKE ? " +
             "AND sms._id=words.source_id AND words.table_to_use=1)";
 
     // Search on the words table but return the rows from the corresponding parts table
@@ -202,7 +202,7 @@ public class MmsSmsProvider extends ContentProvider {
             "(addr.msg_id=pdu._id) AND " +
             "(addr.type=" + PduHeaders.TO + ") AND " +
             "(part.ct='text/plain') AND " +
-            "(index_text MATCH ?) AND " +
+            "(index_text LIKE ?) AND " +
             "(part._id = words.source_id) AND " +
             "(words.table_to_use=2))";
 
@@ -422,8 +422,12 @@ public class MmsSmsProvider extends ContentProvider {
                         sortOrder);
                 break;
             case URI_SEARCH_SUGGEST: {
-                SEARCH_STRING[0] = uri.getQueryParameter("pattern") + '*' ;
-
+                String pattern = uri.getQueryParameter("pattern");
+                if (TextUtils.isEmpty(pattern)) {
+                    return null;
+                } else {
+                    SEARCH_STRING[0] = "%" + pattern + "%";;
+                }
                 // find the words which match the pattern using the snippet function.  The
                 // snippet function parameters mainly describe how to format the result.
                 // See http://www.sqlite.org/fts3.html#section_4_2 for details.
@@ -476,8 +480,11 @@ public class MmsSmsProvider extends ContentProvider {
                             "do not specify sortOrder, selection, selectionArgs, or projection" +
                             "with this query");
                 }
-
-                String searchString = uri.getQueryParameter("pattern") + "*";
+                String pattern = uri.getQueryParameter("pattern");
+                if (TextUtils.isEmpty(pattern)) {
+                    return null;
+                }
+                String searchString = "%" + pattern + "%";
 
                 try {
                     cursor = db.rawQuery(SMS_MMS_QUERY, new String[] { searchString, searchString });
