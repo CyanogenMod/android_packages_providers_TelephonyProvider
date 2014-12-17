19a97a6701d625d5aa803f0fd9b7f1638ee877d5
Roman Birg <roman@cyngn.com>
2014-12-17 10:28:39 -0800

    TelephonyProvider: compile files under src
    Change-Id: Ibc53027466da9f69eb58b3a6b146dd2b1311f425
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- Android.mk
+++ Android.mk
@@ -5,7 +5,7 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_PRIVILEGED_MODULE := true
 
-LOCAL_SRC_FILES := $(call all-subdir-java-files)
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := TelephonyProvider
 LOCAL_CERTIFICATE := platform
