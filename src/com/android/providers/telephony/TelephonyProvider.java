b7d3c99a7b0b954a9af85f1931560ac1f5558cbd
Danny Baumann <dannybaumann@web.de>
2015-03-24 09:11:16 +0100

    Properly upgrade DB
    Change-Id: I5756b77eb706d18ac0518fb878fc25dcc1ce2870

--- src/com/android/providers/telephony/TelephonyProvider.java
+++ src/com/android/providers/telephony/TelephonyProvider.java
@@ -619,6 +619,19 @@ public class TelephonyProvider extends ContentProvider
                 }
                 oldVersion = 16 << 16 | 6;
             }
+            if (oldVersion < (17 << 16 | 6)) {
+                try {
+                    // Try to update the siminfo table. It might not be there.
+                    db.execSQL("ALTER TABLE " + SIMINFO_TABLE +
+                            " ADD COLUMN " + SubscriptionManager.CARRIER_NAME + " TEXT DEFAULT '';");
+                } catch (SQLiteException e) {
+                    if (DBG) {
+                        log("onUpgrade skipping " + SIMINFO_TABLE + " upgrade. " +
+                                " The table will get created in onOpen.");
+                    }
+                }
+                oldVersion = 17 << 16 | 6;
+            }
             if (DBG) {
                 log("dbh.onUpgrade:- db=" + db + " oldV=" + oldVersion + " newV=" + newVersion);
             }
