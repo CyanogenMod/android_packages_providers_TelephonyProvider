29d4eb48d949b3b51b9e89b5f802dce6d85e8fda
ktoonsez <ktoonsez@gmail.com>
2015-03-28 16:47:04 -0700

    SMS: Fix 3rd party sms and mms application support
    * Caf intruduced a new db entry, aosp still uses the old one.
      add back old db entry to allow Google Messager app to function
      without breaking CM MMS app
    
    Change-Id: If590d97f275ceaca3d153eaff3681e4d2b28a833

--- src/com/android/providers/telephony/SmsProvider.java
+++ src/com/android/providers/telephony/SmsProvider.java
@@ -271,7 +271,13 @@ public class SmsProvider extends ContentProvider {
         } else if (qb.getTables().equals(TABLE_SMS)) {
             orderBy = Sms.DEFAULT_SORT_ORDER;
         }
-
+	
+	if (projectionIn != null) {
+		for (int i = 0; i < projectionIn.length; i++) {
+			if (projectionIn[i].equals("sub_id"))
+				projectionIn[i] = "phone_id";
+		}
+	}
         SQLiteDatabase db = mOpenHelper.getReadableDatabase();
         Cursor ret = qb.query(db, projectionIn, selection, selectionArgs,
                               null, null, orderBy);
@@ -604,7 +610,13 @@ public class SmsProvider extends ContentProvider {
                 values = initialValues;
             }
         }
-
+	if (values.containsKey("sub_id"))
+	{
+		int val = values.getAsInteger("sub_id");
+		values.remove("sub_id");
+		values.put("phone_id", val);
+	}
+		
         rowID = db.insert(table, "body", values);
 
         // Don't use a trigger for updating the words table because of a bug
