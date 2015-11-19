1794cb5d2f08b090c373fa144cd85c5b3eb3fc8c
gkipnis <gkipnis@cyngn.com>
2015-11-19 00:16:55 -0800

    Modified notify() functions to return the Uri being modified Added 'blacklist/timestamp' Uri matching to return the timestamp of the last update (insert/update/delete) to the blacklist table when queried.
    Change-Id: I6eff494045e768c6d1a471a79bc2a705114c2027

--- src/com/android/providers/telephony/BlacklistProvider.java
+++ src/com/android/providers/telephony/BlacklistProvider.java
@@ -16,30 +16,32 @@
 
 package com.android.providers.telephony;
 
+import android.content.ContentProvider;
+import android.content.UriMatcher;
+import android.content.SharedPreferences;
+import android.content.Context;
+import android.content.ContentValues;
+import android.content.ContentUris;
+import android.database.MatrixCursor;
+import android.text.format.Time;
 import com.android.internal.telephony.util.BlacklistUtils;
 
 import android.app.backup.BackupManager;
-import android.content.ContentProvider;
-import android.content.ContentUris;
-import android.content.ContentValues;
-import android.content.Context;
-import android.content.UriMatcher;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
 import android.provider.Telephony.Blacklist;
-import android.telephony.PhoneNumberUtils;
-import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 import android.util.Log;
 import android.util.Pair;
 
-import java.util.Locale;
-
 public class BlacklistProvider extends ContentProvider {
     private static final String TAG = "BlacklistProvider";
+    private static final String BLACKLIST_TIMESTAMP_FILE =
+            "telephony_blacklist_timestamp_file";
+    private static final String BLACKLIST_TIMESTAMP = "blacklist_timestamp";
     private static final boolean DEBUG = true;
 
     private static final String DATABASE_NAME = "blacklist.db";
@@ -53,6 +55,9 @@ public class BlacklistProvider extends ContentProvider {
     private static final int BL_NUMBER      = 2;
     private static final int BL_PHONE       = 3;
     private static final int BL_MESSAGE     = 4;
+    private static final int BL_TIMESTAMP   = 5;
+
+    private SharedPreferences mSharedPrefs;
 
     private static final UriMatcher
             sURIMatcher = new UriMatcher(UriMatcher.NO_MATCH);
@@ -63,6 +68,7 @@ public class BlacklistProvider extends ContentProvider {
         sURIMatcher.addURI("blacklist", "bynumber/*", BL_NUMBER);
         sURIMatcher.addURI("blacklist", "phone",      BL_PHONE);
         sURIMatcher.addURI("blacklist", "message",    BL_MESSAGE);
+        sURIMatcher.addURI("blacklist", "timestamp",  BL_TIMESTAMP);
     }
 
     private static class DatabaseHelper extends SQLiteOpenHelper {
@@ -140,8 +146,35 @@ public class BlacklistProvider extends ContentProvider {
     public boolean onCreate() {
         mOpenHelper = new DatabaseHelper(getContext());
         mBackupManager = new BackupManager(getContext());
+        mSharedPrefs = getContext()
+                .getSharedPreferences(BLACKLIST_TIMESTAMP_FILE, Context.MODE_PRIVATE);
         return true;
     }
+
+    private long updateTimestamp() {
+        if (DEBUG) {
+            Log.v(TAG, "updateTimestamp");
+        }
+        SharedPreferences.Editor editor = mSharedPrefs.edit();
+        Time time = new Time();
+        time.setToNow();
+        long now = time.toMillis(true);
+        editor.putLong(BLACKLIST_TIMESTAMP, now);
+        editor.commit();
+        return now;
+    }
+
+    private long getTimestamp() {
+        long timestamp = mSharedPrefs.getLong(BLACKLIST_TIMESTAMP, 0);
+        if (DEBUG) {
+            Log.v(TAG, "getTimestamp: " + timestamp);
+        }
+        if (timestamp == 0) {
+            timestamp = updateTimestamp();
+        }
+        return timestamp;
+    }
+
 
     @Override
     public Cursor query(Uri uri, String[] projection, String selection,
@@ -157,6 +190,8 @@ public class BlacklistProvider extends ContentProvider {
         }
 
         switch (match) {
+            case BL_TIMESTAMP:
+                break;
             case BL_ALL:
                 break;
             case BL_ID:
@@ -185,16 +220,28 @@ public class BlacklistProvider extends ContentProvider {
                 return null;
         }
 
-        if (TextUtils.isEmpty(sortOrder)) {
-            sortOrder = Blacklist.DEFAULT_SORT_ORDER;
+        Cursor cursor;
+        MatrixCursor tsCursor;
+        if (match == BL_TIMESTAMP) {
+            String [] cols = new String [1];
+            Long [] timestamp = new Long [1];
+            cols[0] = "timestamp";
+            timestamp[0] = getTimestamp();
+            tsCursor = new MatrixCursor(cols, 1);
+            tsCursor.addRow(timestamp);
+            cursor = tsCursor;
+        } else {
+            if (TextUtils.isEmpty(sortOrder)) {
+                sortOrder = Blacklist.DEFAULT_SORT_ORDER;
+            }
+
+            SQLiteDatabase db = mOpenHelper.getReadableDatabase();
+            cursor = qb.query(db, projection, selection, selectionArgs, null, null, sortOrder);
         }
 
-        SQLiteDatabase db = mOpenHelper.getReadableDatabase();
-        Cursor ret = qb.query(db, projection, selection, selectionArgs, null, null, sortOrder);
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
 
-        ret.setNotificationUri(getContext().getContentResolver(), uri);
-
-        return ret;
+        return cursor;
     }
 
     @Override
@@ -207,6 +254,8 @@ public class BlacklistProvider extends ContentProvider {
             case BL_ID:
             case BL_NUMBER:
                 return "vnd.android.cursor.item/blacklist-entry";
+            case BL_TIMESTAMP:
+                return "vnd.cyanogenmod.cursor/timestamp";
             default:
                 throw new IllegalArgumentException("Unknown URI " + uri);
         }
@@ -236,7 +285,8 @@ public class BlacklistProvider extends ContentProvider {
         }
 
         if (DEBUG) Log.d(TAG, "inserted " + values + " rowID = " + rowID);
-        notifyChange();
+        notifyChange(uri);
+        updateTimestamp();
 
         return ContentUris.withAppendedId(Blacklist.CONTENT_URI, rowID);
     }
@@ -282,7 +332,8 @@ public class BlacklistProvider extends ContentProvider {
         if (DEBUG) Log.d(TAG, "delete result count " + count);
 
         if (count > 0) {
-            notifyChange();
+            notifyChange(uri);
+            updateTimestamp();
         }
 
         return count;
@@ -345,7 +396,8 @@ public class BlacklistProvider extends ContentProvider {
         if (DEBUG) Log.d(TAG, "Update result count " + count);
 
         if (count > 0) {
-            notifyChange();
+            notifyChange(uri);
+            updateTimestamp();
         }
 
         return count;
@@ -385,7 +437,15 @@ public class BlacklistProvider extends ContentProvider {
     }
 
     private void notifyChange() {
-        getContext().getContentResolver().notifyChange(Blacklist.CONTENT_URI, null);
+        notifyChange(null);
+    }
+
+    private void notifyChange(Uri changeUri) {
+        if (changeUri == null) {
+            getContext().getContentResolver().notifyChange(Blacklist.CONTENT_URI, null);
+        } else {
+            getContext().getContentResolver().notifyChange(changeUri, null);
+        }
         mBackupManager.dataChanged();
     }
 }
