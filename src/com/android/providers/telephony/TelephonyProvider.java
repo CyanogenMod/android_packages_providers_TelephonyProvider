bc4c6dfe0f14e64169fe3e92c931e8ff6604135f
Mengjun Leng <mengju@codeaurora.org>
2016-05-13 15:12:58 +0800

    APN: Add the read only APN feature.
    Add a read only field to the APN database for the read only
    APN feature.
    
    Some operators don't want their APNs to be edited by end user to
    avoid mistaken changes. End user should be able to only view the
    APNs, but not edit.
    
    Change-Id: I75524ecccae33565dcee77832eb7252eab794bc2
    CRs-Fixed: 774852

--- src/com/android/providers/telephony/TelephonyProvider.java
+++ src/com/android/providers/telephony/TelephonyProvider.java
@@ -107,6 +107,8 @@ public class TelephonyProvider extends ContentProvider
     private static final String OTA_UPDATED_APNS_PATH = "misc/apns-conf.xml";
     private static final String OLD_APNS_PATH = "etc/old-apns-conf.xml";
 
+    private static final String READ_ONLY = "read_only";
+
     private static final UriMatcher s_urlMatcher = new UriMatcher(UriMatcher.NO_MATCH);
 
     private static final ContentValues s_currentNullMap;
@@ -316,6 +318,7 @@ public class TelephonyProvider extends ContentProvider
                     MTU + " INTEGER DEFAULT 0," +
                     EDITED + " INTEGER DEFAULT " + UNEDITED + "," +
                     USER_VISIBLE + " BOOLEAN DEFAULT 1," +
+                    READ_ONLY + " BOOLEAN DEFAULT 0," +
                     // Uniqueness collisions are used to trigger merge code so if a field is listed
                     // here it means we will accept both (user edited + new apn_conf definition)
                     // Columns not included in UNIQUE constraint: name, current, edited,
@@ -1095,6 +1098,7 @@ public class TelephonyProvider extends ContentProvider
             addBoolAttribute(parser, "carrier_enabled", map, CARRIER_ENABLED);
             addBoolAttribute(parser, "modem_cognitive", map, MODEM_COGNITIVE);
             addBoolAttribute(parser, "user_visible", map, USER_VISIBLE);
+            addBoolAttribute(parser, "read_only", map, READ_ONLY);
 
             int bearerBitmask = 0;
             String bearerList = parser.getAttributeValue(null, "bearer_bitmask");
